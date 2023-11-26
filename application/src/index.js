a9c2826598381e193314a1cd5964d11de59018be
JuliaNS <proxzyb@gmail.com>
2023-11-27 02:37:27 +0300

    Little interface and script update
    added disappearance of button and awaiting gif; initializing GetAccount function only if provider exists

--- application/src/index.js
+++ application/src/index.js
@@ -4,17 +4,47 @@
 
 import detectEthereumProvider from '@metamask/detect-provider';
 
+
+const ethereumButton = document.querySelector('.enableEthereumButton');
+const showAccount = document.querySelector('.showAccount');
+const waitingcat = document.getElementById("kitty");
+
 const provider = await detectEthereumProvider();
 
 if (provider) {
   startApp(provider);
+
+waitingcat.style.display = 'none';
+ethereumButton.style.display = 'inline';
+ethereumButton.addEventListener('click', () => {
+  getAccount();
+});
+
+async function getAccount() {
+  const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+    .catch((err) => {
+      if (err.code === 4001) {
+        console.log('Please connect to MetaMask.');
+      } else {
+        console.error(err);
+      }
+    });
+  const account = accounts[0];
+  showAccount.innerHTML = 'Your account name is: ' + account;
+}
+
 } else {
   console.log('Please install MetaMask!');
+  waitingcat.style.display = 'none';
+   showAccount.innerHTML = "Are you sure you have MetaMask browser extension?";
+
 }
 
 function startApp(provider) {
   if (provider !== window.ethereum) {
     console.error('Do you have multiple wallets installed?');
+    waitingcat.style.display = 'none';
+    showAccount.innerHTML = "Sorry, multiple wallets aren't supported.";
   }
 }
 
@@ -52,26 +82,4 @@ function handleAccountsChanged(accounts) {
   }
 }
 
-/*********************************************/
-/* Access the user's accounts (per EIP-1102) */
-/*********************************************/
 
-const ethereumButton = document.querySelector('.enableEthereumButton');
-const showAccount = document.querySelector('.showAccount');
-
-ethereumButton.addEventListener('click', () => {
-  getAccount();
-});
-
-async function getAccount() {
-  const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
-    .catch((err) => {
-      if (err.code === 4001) {
-        console.log('Please connect to MetaMask.');
-      } else {
-        console.error(err);
-      }
-    });
-  const account = accounts[0];
-  showAccount.innerHTML = account;
-}
