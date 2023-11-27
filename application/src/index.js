c6005e2da156778180167fd7532896f893a3e6cd
JuliaNS <proxzyb@gmail.com>
2023-11-27 15:40:06 +0300

    Added displaying chainId

--- application/src/index.js
+++ application/src/index.js
@@ -8,6 +8,7 @@ import detectEthereumProvider from '@metamask/detect-provider';
 const ethereumButton = document.querySelector('.enableEthereumButton');
 const showAccount = document.querySelector('.showAccount');
 const waitingcat = document.getElementById("kitty");
+const chainIdSpan = document.getElementById("chId");
 
 const provider = await detectEthereumProvider();
 
@@ -21,16 +22,30 @@ ethereumButton.addEventListener('click', () => {
 });
 
 async function getAccount() {
-  const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+  
+  const [accounts, chainId] = await Promise.all([
+  window.ethereum.request({ method: 'eth_requestAccounts' })
     .catch((err) => {
       if (err.code === 4001) {
         console.log('Please connect to MetaMask.');
+        showAccount.innerHTML = 'Please connect to MetaMask!';
+        chainIdSpan.innerHTML = "";
       } else {
         console.error(err);
+        showAccount.innerHTML = 'Ohh, some error occured. Look: ' + err;
+        chainIdSpan.innerHTML = "";
       }
-    });
+    }),
+    window.ethereum.request({ method: 'eth_chainId' })
+  ]);
   const account = accounts[0];
   showAccount.innerHTML = 'Your account name is: ' + account;
+  
+  if (chainId==="0x1") {
+  chainIdSpan.innerHTML = "You are in Ethereum main net, it's OK.";
+  }
+  else {chainIdSpan.innerHTML = "Do you know you aren't in Ethereum main net? Your chain id is:"+chainId;}
+    
 }
 
 } else {
@@ -45,6 +60,7 @@ function startApp(provider) {
     console.error('Do you have multiple wallets installed?');
     waitingcat.style.display = 'none';
     showAccount.innerHTML = "Sorry, multiple wallets aren't supported.";
+    chainIdSpan.innerHTML = "";
   }
 }
 
@@ -53,6 +69,10 @@ function startApp(provider) {
 /**********************************************************/
 
 const chainId = await window.ethereum.request({ method: 'eth_chainId' });
+if (chainId==="0x1") {
+  chainIdSpan.innerHTML = "You are in Ethereum main net, it's OK.";
+  }
+  else {chainIdSpan.innerHTML = "Do you know you aren't in Ethereum main net? Your chain id is:"+chainId;}
 
 window.ethereum.on('chainChanged', handleChainChanged);
 
@@ -69,6 +89,8 @@ window.ethereum.request({ method: 'eth_accounts' })
   .then(handleAccountsChanged)
   .catch((err) => {
     console.error(err);
+    showAccount.innerHTML = 'Ohh, some error occured. Look: ' + err;
+    chainIdSpan.innerHTML = "";
   });
 
 window.ethereum.on('accountsChanged', handleAccountsChanged);
@@ -76,9 +98,12 @@ window.ethereum.on('accountsChanged', handleAccountsChanged);
 function handleAccountsChanged(accounts) {
   if (accounts.length === 0) {
     console.log('Please connect to MetaMask.');
+    showAccount.innerHTML = 'Please connect to MetaMask!';
+    chainIdSpan.innerHTML = "";
   } else if (accounts[0] !== currentAccount) {
     currentAccount = accounts[0];
-    showAccount.innerHTML = currentAccount;
+    showAccount.innerHTML = 'Your account name is: ' + currentAccount;
+    
   }
 }
 
